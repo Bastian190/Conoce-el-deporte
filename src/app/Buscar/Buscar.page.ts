efaa63c85a4711f0f6fa5a4c87c25c243fe17b00
Bastian <bastianpa10@gmail.com>
2024-10-07 17:38:07 -0300

    barra busqueda listaaa

--- src/app/Buscar/Buscar.page.ts
+++ src/app/Buscar/Buscar.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FirestoreService } from '../servicios/firestore.service';
 import { Equipos } from '../modelos/equipos.models';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'buscar',
@@ -9,16 +10,35 @@ import { Equipos } from '../modelos/equipos.models';
 })
 export class Buscar {
   equipos: Equipos[] = [];
+  equiposFiltrados: Equipos[] = [];
 
-  constructor(private firestoreService:FirestoreService) { this.loadequipos();}
-  loadequipos(){
+  constructor(private firestoreService:FirestoreService,private cdr: ChangeDetectorRef) { this.loadequipos();}
+
+  loadequipos() {
+    // Carga los equipos desde Firestore
     this.firestoreService.getcolleccionChanges<Equipos>('Equipos').subscribe(data => {
       if (data) {
-        this.equipos = data
+        this.equipos = data;
+        this.equiposFiltrados = [...this.equipos];
+        this.cdr.detectChanges();
       }
     });
   }
-  
-  
+  buscarEquipo(event: any) {
+    const valorBusqueda = event.detail.value ? event.detail.value.toLowerCase() : '';  
 
+    console.log('Valor de búsqueda:', valorBusqueda);  
+
+    setTimeout(() => {
+      if (!valorBusqueda || valorBusqueda.trim() === '') {
+        this.equiposFiltrados = [...this.equipos];
+      } else {
+        this.equiposFiltrados = this.equipos.filter(equipo => {
+          return equipo.nombre_fantasia.toLowerCase().includes(valorBusqueda) ||
+                 equipo.tipo_deporte.toLowerCase().includes(valorBusqueda);
+        });
+      }
+      this.cdr.detectChanges();  
+    }, 0);
+  }
 }
